460ca503a08d5d1b5904e3dbd41948c2802bec88
Admin <Admin@192.168.1.24>
2020-04-15 21:40:57 +0200

    modifica modello utente

--- webApp-Battleground/src/main/java/it/dstech/modelli/Utente.java
+++ webApp-Battleground/src/main/java/it/dstech/modelli/Utente.java
@@ -1,9 +1,11 @@
 package it.dstech.modelli;
 
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Utente {
@@ -12,70 +14,64 @@ public class Utente {
 		private String password;
 		private boolean active = false;
 		private int rating = 0;
-		private byte[] image;
+		private Blob image;
 		
+		@OneToMany
 		List<Eroe> listaEroe = new ArrayList<Eroe>();
+		
+		@OneToMany
 		List<Partita> storicoPartite = new ArrayList<Partita>();
-		
-		
-		public Utente(String username, String password) {
-			super();
-			this.username = username;
-			this.password = password;
-		}
-
-		public void creaPartita(){
-			
-		}
-		
-		public Utente(String username, String password, boolean active, byte[] image) {
-			super();
-			this.username = username;
-			this.password = password;
-			this.active = active;
-			this.image = image;
-		}
 		
 		public Utente() {
 			super();
 		}
 
-		public Utente(String username, String password, boolean active) {
+		public Utente(String username, String password, boolean active, int rating, Blob image) {
 			super();
 			this.username = username;
 			this.password = password;
 			this.active = active;
+			this.rating = rating;
+			this.image = image;
 		}
 
 		public String getUsername() {
 			return username;
 		}
+
 		public void setUsername(String username) {
 			this.username = username;
 		}
+
 		public String getPassword() {
 			return password;
 		}
+
 		public void setPassword(String password) {
 			this.password = password;
 		}
+
 		public boolean isActive() {
 			return active;
 		}
+
 		public void setActive(boolean active) {
 			this.active = active;
 		}
-		public byte[] getImage() {
-			return image;
-		}
-		public void setImage(byte[] image) {
-			this.image = image;
+
+		public int getRating() {
+			return rating;
 		}
 
-		@Override
-		public String toString() {
-			return "Utente [username=" + username + ", password=" + password + ", active=" + active + ", image="
-					+ Arrays.toString(image) + "]";
+		public void setRating(int rating) {
+			this.rating = rating;
 		}
-		
+
+		public Blob getImage() {
+			return image;
+		}
+
+		public void setImage(Blob image) {
+			this.image = image;
+		}		
 }
