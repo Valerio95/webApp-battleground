8a4a32432ad5d71b5f26394c78b8d95eb593f02d
Valerio95 <valerioproietti95@outlook.it>
2020-04-15 22:05:53 +0200

    aggiunta eroe e partita

---
+++ webApp-Battleground/src/main/java/it/dstech/modelli/Utente.java
@@ -0,0 +1,77 @@
+package it.dstech.modelli;
+
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Utente {
+		
+		private String username;
+		private String password;
+		private boolean active = false;
+		private int rating = 0;
+		private Blob image;
+		
+		@OneToMany
+		List<Eroe> listaEroe = new ArrayList<Eroe>();
+		
+		@OneToMany
+		List<Partita> storicoPartite = new ArrayList<Partita>();
+		
+		public Utente() {
+			super();
+		}
+
+		public Utente(String username, String password, boolean active, int rating, Blob image) {
+			super();
+			this.username = username;
+			this.password = password;
+			this.active = active;
+			this.rating = rating;
+			this.image = image;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public boolean isActive() {
+			return active;
+		}
+
+		public void setActive(boolean active) {
+			this.active = active;
+		}
+
+		public int getRating() {
+			return rating;
+		}
+
+		public void setRating(int rating) {
+			this.rating = rating;
+		}
+
+		public Blob getImage() {
+			return image;
+		}
+
+		public void setImage(Blob image) {
+			this.image = image;
+		}		
+}
