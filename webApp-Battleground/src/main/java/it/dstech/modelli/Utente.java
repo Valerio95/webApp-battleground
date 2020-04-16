e6dc54891cc23ef6fdc1b7040073677ab8dd02f1
Valerio95 <valerioproietti95@outlook.it>
2020-04-16 12:45:32 +0200

    modifica utente

--- webApp-Battleground/src/main/java/it/dstech/modelli/Utente.java
+++ webApp-Battleground/src/main/java/it/dstech/modelli/Utente.java
@@ -4,11 +4,12 @@ import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 @Entity
 public class Utente {
-		
+		@Id
 		private String username;
 		private String password;
 		private boolean active = false;
