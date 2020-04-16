979fe65002fcebb985a176c501bedfc92efc861a
Valerio95 <valerioproietti95@outlook.it>
2020-04-16 15:53:13 +0200

    creazione gestione

--- webApp-Battleground/src/main/java/it/dstech/gestione/GestioneBattleground.java
+++ webApp-Battleground/src/main/java/it/dstech/gestione/GestioneBattleground.java
@@ -1,45 +1,12 @@
 package it.dstech.gestione;
 
-import java.io.IOException;
-import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import it.dstech.modelli.Utente;
+import javax.persistence.Persistence;
 
+public class GestioneBattleground {
+	private  final long serialVersionUID = 1L;
+	private  EntityManagerFactory emf = Persistence.createEntityManagerFactory("emf");
+	private  EntityManager em = emf.createEntityManager();
 
-@WebServlet(urlPatterns = "/")
-public class GestioneBattleground extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
-		EntityManager em = emf.createEntityManager();
-		Utente utente = new Utente();
-		
-		utente.setUsername(req.getParameter("username"));
-		utente.setPassword(req.getParameter("password"));
-		
-		em.getTransaction().begin();
-		em.persist(utente);
-		em.getTransaction().commit();
-		
-        doGet(req, resp);
-	}
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
-		EntityManager em = emf.createEntityManager();
-
-		List<Utente> listaUtenti = em.createQuery("SELECT u FROM Utente u", Utente.class).getResultList();
-		
-		req.setAttribute("listaUtenti", listaUtenti);
-		req.getRequestDispatcher("Homepage.jsp").forward(req, resp);
-	}
 }
