16ec89727cad1b15d5c6d39ae5dd664ca26b0983
Valerio95 <valerioproietti95@outlook.it>
2020-04-16 16:19:37 +0200

    modifica creazione

--- webApp-Battleground/src/main/java/it/dstech/gestione/GestioneBattleground.java
+++ webApp-Battleground/src/main/java/it/dstech/gestione/GestioneBattleground.java
@@ -4,9 +4,37 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class GestioneBattleground {
-	private  final long serialVersionUID = 1L;
-	private  EntityManagerFactory emf = Persistence.createEntityManagerFactory("emf");
-	private  EntityManager em = emf.createEntityManager();
+import it.dstech.modelli.Eroe;
+import it.dstech.modelli.Partita;
+import it.dstech.modelli.Utente;
 
+public class GestioneBattleground {
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("emf");
+	private static  EntityManager em = emf.createEntityManager();
+
+	public static void creazioneUtente(Utente u) {
+        
+        em.getTransaction().begin();
+        em.persist(u);
+        em.getTransaction().commit();
+        em.close();
+    }
+	public static void creazioneEroe(Eroe e) {
+	       
+        em.getTransaction().begin();
+        em.persist(e);
+        em.getTransaction().commit();
+        em.close();
+    }
+	
+	public static void creazionePartita(Partita p) {
+	       
+        em.getTransaction().begin();
+        em.persist(p);
+        em.getTransaction().commit();
+        em.close();
+    }
+	
+	
+	
 }
